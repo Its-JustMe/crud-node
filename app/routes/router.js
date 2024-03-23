c1cb4711796f568a2957c2b3306768bdd3f26cc3
Arthur Santos <108244695+Its-JustMe@users.noreply.github.com>
2024-03-23 16:51:29 -0300

    .

--- app/routes/router.js
+++ app/routes/router.js
@@ -4,15 +4,15 @@ const controller = require("../controllers/controller");
 const { validationResult } = require("express-validator");
 
 router.get("/",  function (req, res) {
-  res.render("pages/form", { funcao: 'Novo funcionário', acao: 'create', dados: null, listaErros: null });
+  controller.listarFuncionarios(req, res);
 });
 
 router.get("/incluir", function (req, res) {
-  res.render("pages/form", { funcao: 'Novo funcionário', acao: 'create', dados: null, listaErros: null });
+  res.render("pages/form", { funcao: 'Novo funcionário', acao: 'create', dados: {nome_funcionario: '', funcao_funcionario: '', salario_funcionario: ''}, listaErros: null });
 });
 
 router.get("/editar", function (req, res) {
-  res.render("pages/form", { funcao: 'Editar dados do funcionário', acao: 'update', dados: null, listaErros: null });
+  res.render("pages/form", { funcao: 'Editar dados do funcionário', acao: 'update', dados: {nome_funcionario: '', funcao_funcionario: '', salario_funcionario: ''}, listaErros: null });
 });
 
 router.get("/excluir", function (req, res) {
